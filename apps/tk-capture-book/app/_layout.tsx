04be1cadf84c81f741f4850d0486b2cfd31ceba8
tkhwang <tkhwang@gmail.com>
2025-02-03 18:36:56 +0900

    ✨ feat(config/app-router): setup expo app router

---
+++ apps/tk-capture-book/app/_layout.tsx
@@ -0,0 +1,17 @@
+// Import your global CSS file
+import "../global.css";
+
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+    </Stack>
+  );
+}
