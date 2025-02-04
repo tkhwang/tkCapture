7790e9c57619292259774a6289e1f5d943d6ae0d
tkhwang <tkhwang@gmail.com>
2025-02-04 20:08:26 +0900

    🔧 chore(app/eslint): simplify ESLint and Prettier configuration
    - Remove explicit Prettier configuration options
    - Streamline ESLint plugins
    - Keep TanStack Query and Expo recommended configurations

--- apps/tk-capture-book/.eslintrc.js
+++ apps/tk-capture-book/.eslintrc.js
@@ -1,22 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: ["universe/native", "plugin:@tanstack/query/recommended"],
-  plugins: ["react-hooks", "@typescript-eslint", "prettier"],
+  extends: ["universe/native", "prettier", "plugin:@tanstack/query/recommended"],
+  plugins: ["@typescript-eslint", "prettier"],
 
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        printWidth: 120,
-        tabWidth: 2,
-        useTabs: false,
-        semi: true,
-        singleQuote: false,
-        trailingComma: "all",
-        bracketSpacing: true,
-        arrowParens: "always",
-      },
-    ],
+    "prettier/prettier": "error",
   },
 };
