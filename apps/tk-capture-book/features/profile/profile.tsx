b6753970afb5a038b6d5a648621d551b83959f1f
tkhwang <tkhwang@gmail.com>
2025-03-29 23:45:11 +0900

    ✨ feat(auth): display user's provider icon
    - add conditional rendering for user's provider icon
    - import AntDesign from '@expo/vector-icons'
    - remove unnecessary import line for MaterialIcons
    - add conditions for "apple" and "google" providers

--- apps/tk-capture-book/features/profile/profile.tsx
+++ apps/tk-capture-book/features/profile/profile.tsx
@@ -1,5 +1,4 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import React from "react";
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
 import { View, Text, TouchableOpacity } from "react-native";
 
 import { useAuth } from "@/providers/auth-provider";
@@ -16,13 +15,18 @@ export function Profile({ onLogout }: ProfileProps) {
       <View className="flex-row items-center justify-between">
         <View className="flex-row items-center gap-3">
           <View className="items-center justify-center w-12 h-12 bg-white rounded-full">
-            <MaterialIcons name="person" size={28} color="#4B5563" />
+            {user?.provider === "apple" ? (
+              <AntDesign name="apple1" size={28} color="#000000" />
+            ) : user?.provider === "google" ? (
+              <AntDesign name="google" size={28} color="#4285F4" />
+            ) : (
+              <MaterialIcons name="person" size={28} color="#4B5563" />
+            )}
           </View>
           <View>
             <Text className="text-xl font-bold text-black">{user?.name ?? ""}</Text>
           </View>
         </View>
-
         <TouchableOpacity className="px-4 py-4 bg-gray-200 rounded-full" onPress={onLogout}>
           <Text className="font-semibold text-primary">로그아웃</Text>
         </TouchableOpacity>
