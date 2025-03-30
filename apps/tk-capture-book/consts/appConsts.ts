351b8f1728f68e0bc6a7aa1ed7948d6a533eee18
tkhwang <tkhwang@gmail.com>
2025-03-30 19:23:46 +0900

    🔧 config(version): update app and playstore version to 1.26.1
    - update version code in android/app/build.gradle
    - update app version and playstore version in appConsts
    - update CFBundleShortVersionString in ios/tkCaptureBook/Info.plist

--- apps/tk-capture-book/consts/appConsts.ts
+++ apps/tk-capture-book/consts/appConsts.ts
@@ -6,8 +6,8 @@ export const APP_NAME = "tkCaptureBook";
 /*
  *  APP VERSION
  */
-export const APP_VERSION = "1.25.1";
-export const PLAYSTORE_VERSION = "25";
+export const APP_VERSION = "1.26.1";
+export const PLAYSTORE_VERSION = "26";
 export const VERSION = `${APP_VERSION} (${PLAYSTORE_VERSION})`;
 
 /*
