2c7f6a8323f4bd4e205155cf782c257bd32b4201
tkhwang <tkhwang@gmail.com>
2025-03-30 19:19:55 +0900

    🐛 fix(env): use process.env for environment variables and implement checks
    - previous refactoring doesn't work for eas build
    - increase APP_VERSION to "1.25.1" and PLAYSTORE_VERSION to "25"
    - remove use of validateAndLoadEnv() function
    - use process.env directly for reading environment variables
    - throw error if required environment variables are not set
    - log the value of each loaded environment variable

--- apps/tk-capture-book/consts/appConsts.ts
+++ apps/tk-capture-book/consts/appConsts.ts
@@ -1,5 +1,3 @@
-import { validateAndLoadEnv } from "@/utils/env";
-
 /*
  *  APP NAME
  */
@@ -8,23 +6,68 @@ export const APP_NAME = "tkCaptureBook";
 /*
  *  APP VERSION
  */
-export const APP_VERSION = "1.24.1";
-export const PLAYSTORE_VERSION = "24";
+export const APP_VERSION = "1.25.1";
+export const PLAYSTORE_VERSION = "25";
 export const VERSION = `${APP_VERSION} (${PLAYSTORE_VERSION})`;
 
 /*
  *  Environment variable
  */
-const env = validateAndLoadEnv();
+export const NAVER_CLIENT_ID = process.env.EXPO_PUBLIC_NAVER_CLIENT_ID;
+if (!NAVER_CLIENT_ID) {
+  throw new Error("EXPO_PUBLIC_NAVER_CLIENT_ID is not set");
+} else {
+  console.log(`[+][const] NAVER_CLIENT_ID: ${NAVER_CLIENT_ID}`);
+}
 
-export const SUPABASE_URL = env.EXPO_PUBLIC_SUPABASE_URL;
-export const SUPABASE_ANON_KEY = env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
-export const GOOGLE_WEB_CLIENT_ID = env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID;
-export const GOOGLE_IOS_CLIENT_ID = env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID;
-export const GOOGLE_CLOUD_API_KEY = env.EXPO_PUBLIC_GOOGLE_CLOUD_API_KEY;
-export const NAVER_CLIENT_ID = env.EXPO_PUBLIC_NAVER_CLIENT_ID;
-export const NAVER_CLIENT_SECRET = env.EXPO_PUBLIC_NAVER_CLIENT_SECRET;
-export const KAKAO_REST_API_KEY = env.EXPO_PUBLIC_KAKAO_REST_API_KEY;
+export const NAVER_CLIENT_SECRET = process.env.EXPO_PUBLIC_NAVER_CLIENT_SECRET;
+if (!NAVER_CLIENT_SECRET) {
+  throw new Error("EXPO_PUBLIC_NAVER_CLIENT_SECRET is not set");
+} else {
+  console.log(`[+][const] NAVER_CLIENT_SECRET: ${NAVER_CLIENT_SECRET}`);
+}
+
+export const KAKAO_REST_API_KEY = process.env.EXPO_PUBLIC_KAKAO_REST_API_KEY;
+if (!KAKAO_REST_API_KEY) {
+  throw new Error("EXPO_PUBLIC_KAKAO_REST_API_KEY is not set");
+} else {
+  console.log(`[+][const] KAKAO_REST_API_KEY: ${KAKAO_REST_API_KEY}`);
+}
+
+export const GOOGLE_CLOUD_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_CLOUD_API_KEY;
+if (!GOOGLE_CLOUD_API_KEY) {
+  throw new Error("EXPO_PUBLIC_GOOGLE_CLOUD_API_KEY is not set");
+} else {
+  console.log(`[+][const] GOOGLE_CLOUD_API_KEY: ${GOOGLE_CLOUD_API_KEY}`);
+}
+
+export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+if (!SUPABASE_URL) {
+  throw new Error("EXPO_PUBLIC_SUPABASE_URL is not set");
+} else {
+  console.log(`[+][const] SUPABASE_URL: ${SUPABASE_URL}`);
+}
+
+export const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+if (!SUPABASE_ANON_KEY) {
+  throw new Error("EXPO_PUBLIC_SUPABASE_ANON_KEY is not set");
+} else {
+  console.log(`[+][const] SUPABASE_ANON_KEY: ${SUPABASE_ANON_KEY}`);
+}
+
+export const GOOGLE_WEB_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID;
+if (!GOOGLE_WEB_CLIENT_ID) {
+  throw new Error("EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID is not set");
+} else {
+  console.log(`[+][const] GOOGLE_WEB_CLIENT_ID: ${GOOGLE_WEB_CLIENT_ID}`);
+}
+
+export const GOOGLE_IOS_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID;
+if (!GOOGLE_IOS_CLIENT_ID) {
+  throw new Error("EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID is not set");
+} else {
+  console.log(`[+][const] GOOGLE_IOS_CLIENT_ID: ${GOOGLE_IOS_CLIENT_ID}`);
+}
 
 /*
  *  Application consts
