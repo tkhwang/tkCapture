77835d53b5bdf1ab30caed0bbbf0094c6515017e
tkhwang <tkhwang@gmail.com>
2025-03-30 12:33:13 +0900

    🔧 config(version): update app and playstore version
    - increase versionCode from 11 to 21
    - upgrade versionName from "1.11.1" to "1.21.1"
    - update APP_VERSION and PLAYSTORE_VERSION constants

--- apps/tk-capture-book/consts/appConsts.ts
+++ apps/tk-capture-book/consts/appConsts.ts
@@ -6,8 +6,8 @@ export const APP_NAME = "tkCaptureBook";
 /*
  *  APP VERSION
  */
-export const APP_VERSION = "1.11.1";
-export const PLAYSTORE_VERSION = "11";
+export const APP_VERSION = "1.21.1";
+export const PLAYSTORE_VERSION = "21";
 export const VERSION = `${APP_VERSION} (${PLAYSTORE_VERSION})`;
 
 /*
