c63cbae4ec8524319722ddf247fb7d9518e63d1c
tkhwang <tkhwang@gmail.com>
2025-04-15 21:22:53 +0900

    🔧 config(version): update app version to 1.34.1
    - bump up versionCode from 33 to 34 in build.gradle
    - update APP_VERSION and PLAYSTORE_VERSION in appConsts.ts
    - change CFBundleShortVersionString in Info.plist to 1.34.1

--- apps/tk-capture-book/consts/appConsts.ts
+++ apps/tk-capture-book/consts/appConsts.ts
@@ -6,8 +6,8 @@ export const APP_NAME = "tkCaptureBook";
 /*
  *  APP VERSION
  */
-export const APP_VERSION = "1.33.1";
-export const PLAYSTORE_VERSION = "33";
+export const APP_VERSION = "1.34.1";
+export const PLAYSTORE_VERSION = "34";
 export const VERSION = `${APP_VERSION} (${PLAYSTORE_VERSION})`;
 
 /*
