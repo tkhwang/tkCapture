737b0135f4561e0dd91c669d5099812f415aa7eb
tkhwang <tkhwang@gmail.com>
2025-06-03 17:44:27 +0900

    🔧 build(config): update app version to 1.37.1
    - updated version code in android build.gradle
    - updated Info.plist for ios
    - updated version constants in appConsts.ts

--- apps/tk-capture-book/consts/appConsts.ts
+++ apps/tk-capture-book/consts/appConsts.ts
@@ -8,8 +8,8 @@ export const APP_NAME = "tkCaptureBook";
 /*
  *  APP VERSION
  */
-export const APP_VERSION = "1.36.1";
-export const PLAYSTORE_VERSION = "36";
+export const APP_VERSION = "1.37.1";
+export const PLAYSTORE_VERSION = "37";
 export const VERSION = `${APP_VERSION} (${PLAYSTORE_VERSION})`;
 
 /*
