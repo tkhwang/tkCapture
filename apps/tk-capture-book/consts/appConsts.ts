44f490c5936a75ec0fb4b66076daa76a04098caa
tkhwang <tkhwang@gmail.com>
2025-06-04 22:34:38 +0900

    🔧 build(version):  update app version to 1.45.1
    - update version code to 45 in android and app constants file
    - update short version string in iOS Info.plist

--- apps/tk-capture-book/consts/appConsts.ts
+++ apps/tk-capture-book/consts/appConsts.ts
@@ -6,8 +6,8 @@ export const APP_NAME = "tkCaptureBook";
 /*
  *  APP VERSION
  */
-export const APP_VERSION = "1.40.1";
-export const PLAYSTORE_VERSION = "40";
+export const APP_VERSION = "1.45.1";
+export const PLAYSTORE_VERSION = "45";
 export const VERSION = `${APP_VERSION} (${PLAYSTORE_VERSION})`;
 
 /*
