75a66afbc37d7e327f7832f3a71f2ae426381bc8
tkhwang <tkhwang@gmail.com>
2025-06-07 22:36:16 +0900

    🔧 config(version): update app version to 1.50.1
    - update versionCode to 50 in Android-specific config
    - update APP_VERSION and PLAYSTORE_VERSION in appConsts.ts
    - update CFBundleShortVersionString in iOS-specific config

--- apps/tk-capture-book/consts/appConsts.ts
+++ apps/tk-capture-book/consts/appConsts.ts
@@ -6,8 +6,8 @@ export const APP_NAME = "tkCaptureBook";
 /*
  *  APP VERSION
  */
-export const APP_VERSION = "1.49.1";
-export const PLAYSTORE_VERSION = "49";
+export const APP_VERSION = "1.50.1";
+export const PLAYSTORE_VERSION = "50";
 export const VERSION = `${APP_VERSION} (${PLAYSTORE_VERSION})`;
 
 /*
