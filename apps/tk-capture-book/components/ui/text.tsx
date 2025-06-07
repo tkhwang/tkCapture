b9381d08b193493cdce62f643f49ff2231e47900
tkhwang <tkhwang@gmail.com>
2025-06-07 13:39:12 +0900

    ✨ feat(book): add new Badge component and refactor Text component
    - add a new ui component "Badge"
    - refactor "Text" component with new approach
    - add package "@rn-primitives/slot": "^1.2.0" in "tk-capture-book" dependencies
    - update yarn.lock with the new package information.

--- apps/tk-capture-book/components/ui/text.tsx
+++ apps/tk-capture-book/components/ui/text.tsx
@@ -1,50 +1,29 @@
 import * as React from "react";
 
-import { Text as RNText, TextProps as RNTextProps } from "react-native";
+import { Text as RNText } from "react-native";
 
-import { cva, type VariantProps } from "class-variance-authority";
+import * as Slot from "@rn-primitives/slot";
 
 import { cn } from "@/lib/utils";
 
-const textVariants = cva("text-foreground", {
-  variants: {
-    variant: {
-      default: "text-foreground",
-      heading: "text-foreground font-bold",
-      title: "text-foreground font-semibold",
-      muted: "text-muted-foreground",
-      destructive: "text-destructive",
-    },
-    size: {
-      default: "text-base",
-      xs: "text-xs",
-      sm: "text-sm leading-4",
-      base: "text-base",
-      lg: "text-lg",
-      xl: "text-xl",
-      "2xl": "text-2xl",
-      "3xl": "text-3xl",
-      "4xl": "text-4xl",
-    },
-  },
-  defaultVariants: {
-    variant: "default",
-    size: "default",
-  },
-});
+const TextClassContext = React.createContext<string | undefined>(undefined);
 
-export interface TextProps extends RNTextProps, VariantProps<typeof textVariants> {
-  className?: string;
+function Text({
+  className,
+  asChild = false,
+  ...props
+}: React.ComponentProps<typeof RNText> & {
+  ref?: React.RefObject<RNText>;
+  asChild?: boolean;
+}) {
+  const textClass = React.useContext(TextClassContext);
+  const Component = asChild ? Slot.Text : RNText;
+  return (
+    <Component
+      className={cn("text-base text-foreground web:select-text", textClass, className)}
+      {...props}
+    />
+  );
 }
 
-const Text = React.forwardRef<React.ElementRef<typeof RNText>, TextProps>(
-  ({ className, variant, size, ...props }, ref) => {
-    return (
-      <RNText ref={ref} className={cn(textVariants({ variant, size, className }))} {...props} />
-    );
-  },
-);
-
-Text.displayName = "Text";
-
-export { Text, textVariants };
+export { Text, TextClassContext };
